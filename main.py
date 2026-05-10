1e27ef8d4c45eb8eb77a2be0ba6b4dddb7aa5811
Gleb Krotov <iotajot@gmail.com>
2026-05-10 13:26:01 +0500

    New change on the main course

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 class Comment:
     def __init__(self, text):
         self.text = text
-        self.votes_qty = 0
 
-    def upvote(self):
-        self.votes_qty += 1
+    @staticmethod
+    def merge_comments(first_comment, second_comment):
+        return f"{first_comment} {second_comment}"
 
 
-first_comment = Comment("First comment")
+my_comment = Comment("My comment")
 
-print(first_comment.text)
-print(first_comment.votes_qty)
+m_1 = Comment.merge_comments("Hello", "World")
+print(m_1)
 
-first_comment.upvote()
-
-print(first_comment.votes_qty)
-
+m_2 = my_comment.merge_comments("Hi", "World")
+print(m_2)
